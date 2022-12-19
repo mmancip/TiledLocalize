19e2162d411a62ca907e421c4eff0ffea9aae83b
Martial Mancip <martial.mancip@maisondelasimulation.fr>
2022-12-19 14:30:52 +0100

    Add YOLO definition from one nodes.json in input tiledset.json.

---
+++ update_tiledset.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+
+with open("nodes.json",'r') as nodesf:
+    nodes=json.load(nodesf)
+
+with open("tiledset.json") as nodes_json:
+    tiles   = json.load(nodes_json)
+    
+i=0
+for tile in tiles["nodes"]:
+   tile["comment"]=tile["comment"]+" YOLO="+str(nodes["YOLO"][str(i)])
+   i=i+1 
+
+
+with open("tiledset_.json",'w') as tiledset_f:
+        tiledset_f.write(json.dumps(tiles))
